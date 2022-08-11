0f5b9ea0506b14bf5e4723fb8be0363367a8702b
Shrutibeladia <shrutibeladi21@gmail.com>
2022-08-11 22:53:28 +0530

    commit seventeen

--- js/script.js
+++ js/script.js
@@ -11,3 +11,42 @@ menuToggle.addEventListener('click',function(){
         menuToggle.innerHTML='<i class="fas fa-bars"></i>'
     }
 })
+
+
+console.log("hello")
+const xhr = new XMLHttpRequest();
+const url = 'https://jsonplaceholder.typicode.com/posts?utm_source=Mailerlite&utm_medium=E-mail&utm_campaign=Test%20Series&utm_term=2022-08-09';
+
+xhr.open('GET',url)
+xhr.onreadystatechange = ()=>{
+    if(xhr.readyState === 4 && xhr.status === 200 ){
+        const objects = JSON.parse(xhr.responseText)
+        var output = ''
+        objects.forEach(object => {
+            document.querySelector('.col').appendChild(addCard(object))
+        });
+        console.log(output)
+    }
+}
+xhr.send()
+
+function addCard(object){
+    const col = document.createElement('div')
+    col.setAttribute('class','post')
+    const card = document.createElement('div')
+    card.setAttribute('class','post-info')
+    const h2 = document.createElement('h2')
+    h2.textContent = object.id
+    const h3 = document.createElement('h3')
+    h3.textContent = object.title
+    const P = document.createElement('P')
+    P.textContent = object.body
+    card.appendChild(h2)
+    card.appendChild(h3)
+    card.appendChild(P)
+    col.appendChild(card)
+
+    return col
+
+}
+
